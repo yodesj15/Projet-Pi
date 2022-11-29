3df180afa6fd319664ca5c49c2f0cdf33e1adbe8
yodesj15 <102743437+yodesj15@users.noreply.github.com>
2022-11-29 13:51:46 -0500

    Update classe_lidar.py

--- Sprint 3/classe_lidar.py
+++ Sprint 3/classe_lidar.py
@@ -1,3 +1,10 @@
+# Auteurs                 Date              Commentaires
+#                          29 novembre 2022  Projet Intégration,
+# BOUTIN-DUFOUR Anthony
+# BOUCHER-BEAUDIN Erick
+# DESJARDINS Yoan
+# BERGEVIN Émeric
+#
 from pkgutil import get_data
 import PyLidar3
 import time,math
@@ -8,7 +15,7 @@ class Lidar:
         self.portLidar = port #"/dev/ttyUSB1"
         self.Obj = PyLidar3.YdLidarX4(self.portLidar)
         self.dataTab = []
-        self.__done = False
+        self.done = False
         self.robot = robot
         self.nbExecution = 0
         self.tab_p_x= [] # a utilisé pour reprensété lidar dans map en X
@@ -16,34 +23,16 @@ class Lidar:
         
         
     def getData(self):
-        distance_proche = 175 # 600 proximité et 175 limite
-        i = 0
         if(self.Obj.Connect()):
             gen = self.Obj.StartScanning()
-            while not self.__done:
+            while not self.done:
 
                 self.dataTab = next(gen)
                 
-                #if self.nbExecution == 0:
-                    #self.nbExecution += 1
                 self.tab_p_x, self.tab_p_y = self.trouver_distance_angle(self.dataTab)
                 
-                time.sleep(0.01) #1
-                #print(self.dataTab[179])
-                if (self.dataTab[179] < distance_proche and self.dataTab[179] != 0):
-                    i = i + 1
-                    print('Objet devant! oups ' + str(i))
-                    self.robot.moteur.fonction = False
-                    self.robot.moteur.freiner() 
-                    
-                
-                #for d in self.dataTab:
-                #    if(int(self.dataTab[d]) < 50 and int(self.dataTab[d]) != 0):
-                #        if int(d) == 180:#devant degree 315 a 45 #cote droit 46 a 135#cote droit 136 a 225#cote droit 226 a 315
-                #            i = i + 1
-                #            print('Objet devant! oups ' + str(i))
-                #            self.robot.moteur.fonction = False
-                #            self.robot.moteur.freiner()            
+                time.sleep(0.01) 
+                       
             self.Obj.StopScanning()
             
     
@@ -66,17 +55,12 @@ class Lidar:
             point = (math.cos(math.radians(angle))) * distance
             
         return point
+    
+    
             
-            
-#         else:
-#             print("Error connecting to device")
     def reset_lidar(self):
         self.Obj.Reset()
         
     def disconnect(self):
-            # self.Obj.Reset()
-        
-            self.Obj.StopScanning()
             self.Obj.Disconnect()
-
 
